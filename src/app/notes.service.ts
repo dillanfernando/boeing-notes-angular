b0950f952dbf9462d0985c8c2962ce03bd9cc610
dillanfernando <dfernando@gmail.com>
2018-05-07 03:01:14 -0700

    cleaned up the code

--- src/app/notes.service.ts
+++ src/app/notes.service.ts
@@ -1,25 +1,23 @@
 import { Injectable } from '@angular/core';
-
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import {  catchError, map, tap } from 'rxjs/operators';
-
 import * as Rx from 'rxjs';
-
 import { INote } from './note';
-
-
-
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class NotesService {
   private httpHeaderOptions = { headers: new HttpHeaders().set('Content-Type', 'application/json') };
-  
+  private  REST_URI = '/services/notes/';
+  private  GET_URI = this.REST_URI + '/get';
+  private  ADD_URI = this.REST_URI + '/add';
+
   constructor(private http : HttpClient) { }
 
   getNotes (): Rx.Observable<INote[]> {
-    return this.http.get<INote[]>('/services/notes/get', this.httpHeaderOptions)
+    return this.http.get<INote[]>(this.GET_URI, this.httpHeaderOptions)
     .pipe(
       tap(notes => console.log(`retrieved notes`)),
       catchError(this.handleError('getNotes'))
@@ -28,8 +26,8 @@ export class NotesService {
 
 
  addNote (noted: INote): Rx.Observable<INote> {
-  return this.http.post<INote>('/services/notes/add', noted, this.httpHeaderOptions).pipe(
-    tap((note: INote) => console.log(`added note w/ title=${noted.subject}`)),
+  return this.http.post<INote>(this.ADD_URI, noted, this.httpHeaderOptions).pipe(
+    tap((note: INote) => console.log(`added note with subject=${noted.subject}`)),
     catchError(this.handleError<INote>('addNote'))
   );
 }
